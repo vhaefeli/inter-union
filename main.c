122da0e3ea8ef2b5e47708ca527017702edaa918
Vanessa Haefeli <vanessahaefeli@Mac-de-Vanessa.local>
2022-01-31 13:02:01 +0100

    modif main

--- main.c
+++ main.c
@@ -3,7 +3,8 @@ void	ft_union(const char *s1, const char *s2);
 
 int main(int argc, char **argv )
 {
-	argc = 3;
+	if (argc != 3)
+		return (0);
 	inter(argv[1], argv[2]);
 	ft_union(argv[1], argv[2]);
 	return (0);
